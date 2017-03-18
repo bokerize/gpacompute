037e4dbcf72952f918a6d201178032f9c2fc233d
jericho <boker_boker@yahoo.com>
2017-03-18 17:29:39 +0800

    android studio generated project

---
+++ app/src/main/java/com/bok/gpacomputer/GpaList.java
@@ -0,0 +1,13 @@
+package com.bok.gpacomputer;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class GpaList extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_gpa_list);
+    }
+}
